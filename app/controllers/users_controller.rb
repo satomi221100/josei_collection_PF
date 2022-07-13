e15beaf0bc91d31b1f26f154e2ef32eaa35e0432
satomi221100 <marin221100@gmail.com>
2022-07-13 13:32:17 +0000

    second

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < ApplicationController
+  
+  def new
+  end
+
+  def index
+    @users=User.all
+    @user=current_user
+    @book=Book.new
+  end
+
+  def show
+    @user=User.find(params[:id])
+    @book=Book.new
+    @books=@user.books
+  end
+
+  def edit
+    @user=User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+    redirect_to user_path(current_user)
+    end
+  end
+
+  def update
+     @user=User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="user was successfully created."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+   private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+  
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+end
